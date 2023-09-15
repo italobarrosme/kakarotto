323643490f2ff809bc81404b4a632c6faed68ee7
italobarrosme <italobarros.me@gmail.com>
2023-09-15 10:31:03 -0300

    feat: semantic html pagenotfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,9 +12,9 @@ const NotFound = () => {
       <section className="flex h-full w-full items-center p-16">
         <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
           <div className="max-w-md text-center">
-            <h2 className="mb-8 text-9xl font-extrabold text-brand-primary">
+            <h1 className="mb-8 text-9xl font-extrabold text-brand-primary">
               <span className="sr-only">Error</span>404
-            </h2>
+            </h1>
             <p className="text-2xl font-semibold md:text-3xl">
               Sorry, we couldnt find this page.
             </p>
